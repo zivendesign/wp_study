b6063b29a3733d0cbe341be43038388afe6a4533
zivendesign <ziven.design@gmail.com>
2016-01-24 18:23:33 +0700

    Hello World Plugin

---
+++ wp-content/plugins/helloworld/helloworld.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hello World
+Plugin URI: http://google.com/
+Description: Hello World
+Version: 1.1
+Author: Ziven
+Author URI: http://wordpress.org/
+License: GPLv2 or later
+Text Domain: Hello World
+*/
